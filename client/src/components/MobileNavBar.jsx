4261f8b12d21df4ddb6d60217387236e2c1f5295
Rohit Kumar <kumarguptarohit2005@gmail.com>
2025-04-16 21:15:49 +0530

    navbar error fixed

--- client/src/components/MobileNavBar.jsx
+++ client/src/components/MobileNavBar.jsx
@@ -4,32 +4,38 @@ import NavBar from '../common/NavBar'
 import { IoClose } from 'react-icons/io5'
 import navpic from '../assets/images/b.jpg'
 
-const MobileNavBar = ({close , darkMode , setDarkMode}) => {
-  return (
-        <div className={`${darkMode ? 'bg-[#161B22]' : 'bg-[#54B689]'} p-2 text-white`}>
-        <div className='flex items-center justify-between gap-4'>
-            <h1 className='text-2xl font-bold'>Rohit Kumar</h1>
-            <button onClick={close}>
-                <IoClose size={22}/>
-            </button>
+const MobileNavBar = ({ close, darkMode, setDarkMode }) => {
+    return (
+
+        <div className='sticky top-0 left-0 right-0 bottom-0 z-50'>
+
+            <div className={`${darkMode ? 'bg-[#161B22]' : 'bg-[#54B689]'} text-white p-3 grid gap-2`}>
+                <div className='flex items-center justify-between gap-4'>
+                    <h1 className='text-2xl font-bold'>Rohit Kumar</h1>
+                    <button onClick={close}>
+                        <IoClose size={22} />
+                    </button>
+                </div>
+
+                <div className='w-full h-full flex flex-col gap-3 items-center justify-center'>
+
+                    <div className='bg-white w-30 h-30 rounded-full'>
+                        <img src={navpic} alt="navpic" className='w-full h-full rounded-full object-cover' />
+                    </div>
+
+                    <p className='text-justify'>Hi, my name is Rohit Kumar and I’m a web developer. Welcome to my personal website!</p>
+
+                    <SocialLinks />
+
+                    <NavBar setDarkMode={setDarkMode} darkMode={darkMode} close={close} />
+
+                </div>
+
+            </div>
+
         </div>
 
-    <div className='w-full h-full flex flex-col gap-3 items-center justify-center'>
-
-        <div className='bg-white w-30 h-30 rounded-full'>
-            <img src={navpic} alt="navpic" className='w-full h-full rounded-full object-cover'/>
-        </div>
-
-        <p className='text-justify'>Hi, my name is Rohit Kumar and I’m a web developer. Welcome to my personal website!</p>
-
-        <SocialLinks />
-
-        <NavBar setDarkMode={setDarkMode} darkMode={darkMode} close={close}/>
-
-    </div>
-
-</div>
-  )
+    )
 }
 
 export default MobileNavBar
